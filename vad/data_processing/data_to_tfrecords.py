d5f1b656e628727bf33a4e21307151f8c054d955
yuri20198 <malinhmh199495@gmail.com>
2020-10-31 11:50:46 +0800

    tf.compat to data_to_tfrecords

--- vad/data_processing/data_to_tfrecords.py
+++ vad/data_processing/data_to_tfrecords.py
@@ -11,27 +11,15 @@ from vad.data_processing.data_iterator import split_data, slice_iter
 from vad.data_processing.feature_extraction import extract_features
 
 
-flags.DEFINE_string('data_dir',
-                    '/home/filippo/datasets/LibriSpeech/',
-                    'data directory path')
-flags.DEFINE_string('data_split',
-                    '0.7/0.15',
-                    'train/val split')
-flags.DEFINE_integer('seq_len',
-                     1024,
-                     'segment length')
-flags.DEFINE_integer('num_shards',
-                     256,
-                     'number of tfrecord files')
-flags.DEFINE_boolean('debug',
-                     False,
-                     'debug for a few samples')
-flags.DEFINE_string('data_type',
-                    'trainvaltest',
-                    'data types to write into tfrecords')
+flags.DEFINE_string(name='data_dir', default='../LibriSpeech/', help='data directory path')
+flags.DEFINE_string(name='data_split', default='0.7/0.15', help='train/val split')
+flags.DEFINE_integer(name='seq_len', default=1024, help='segment length')
+flags.DEFINE_integer(name='num_shards', default=256, help='number of tfrecord files')
+flags.DEFINE_boolean(name='debug', default=False, help='debug for a few samples')
+flags.DEFINE_string(name='data_type', default='trainvaltest', help='data types to write into tfrecords')
 
 FLAGS = flags.FLAGS
-tf.logging.set_verbosity(tf.logging.INFO)
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 
 
 def int64_feature(value):
@@ -98,7 +86,7 @@ def pool_create_tf_example(args):
 
 def write_tfrecords(path, dataiter, num_shards=256, nmax=-1):
     writers = [
-        tf.python_io.TFRecordWriter('{}{:05d}_{:05d}.tfrecord'.format(path, i, num_shards)) for i in range(num_shards)
+        tf.io.TFRecordWriter('{}{:05d}_{:05d}.tfrecord'.format(path, i, num_shards)) for i in range(num_shards)
     ]
     print('\nWriting to output path: {}'.format(path))
     pool = multiprocessing.Pool()
@@ -131,8 +119,8 @@ def create_tfrecords(data_dir,
     np.random.seed(0)
 
     output_path = os.path.join(data_dir, 'tfrecords/')
-    if not tf.gfile.IsDirectory(output_path):
-        tf.gfile.MakeDirs(output_path)
+    if not tf.io.gfile.isdir(output_path):
+        tf.io.gfile.makedirs(output_path)
 
     # Data & label directories
     label_dir = os.path.join(data_dir, 'labels/')
@@ -152,15 +140,15 @@ def create_tfrecords(data_dir,
     if 'train' in data_type:
         print('\nWriting train tfrecords ...')
         train_path = os.path.join(output_path, 'train/')
-        if not tf.gfile.IsDirectory(train_path):
+        if not tf.io.gfile.isdir(train_path):
             tf.gfile.MakeDirs(train_path)
         write_tfrecords(train_path, train_it, num_shards, nmax=nmax)
 
     if 'val' in data_type:
         print('\nWriting val tfrecords ...')
         val_path = os.path.join(output_path, 'val/')
-        if not tf.gfile.IsDirectory(val_path):
-            tf.gfile.MakeDirs(val_path)
+        if not tf.io.gfile.isdir(val_path):
+            tf.io.gfile.makedirs(val_path)
         write_tfrecords(val_path, val_it, num_shards, nmax=nmax)
 
     if 'test' in data_type:
@@ -181,5 +169,5 @@ def main(_):
 
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
     app.run(main)
